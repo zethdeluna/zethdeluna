e9ca3d94c311023627e002c8f5f7486f08a46e2b
zethdeluna <zethdeluna@users.noreply.github.com>
2024-12-07 19:49:56 -0800

    home page

---
+++ src/components/pages/home/hero.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const HomeHero = () => {
+    return (
+        <section className="hero">
+            <div className="container">
+
+                <article>
+                    <h1>Zeth De Luna</h1>
+                </article>
+
+            </div>
+        </section>
+    );
+};
+
+export default HomeHero;
