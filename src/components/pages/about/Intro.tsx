6354df9bbe916dc90e6301f7922bb47e5ca41b9d
zethdeluna <zethdeluna@users.noreply.github.com>
2026-02-17 12:21:19 -0800

    intro content update

--- src/components/pages/about/Intro.tsx
+++ src/components/pages/about/Intro.tsx
@@ -9,7 +9,7 @@ const Intro = () => {
 
 					<div className="about-me">
 						<h2 className="heading-4">About Me</h2>
-						<p>I'm Zeth, a frontend developer with years of experience crafting engaging sites through the magical languages of HTML, CSS, JavaScript, PHP, and a sprinkle of React JavaScript/TypeScript. I love taking beautiful, aesthetically pleasing designs and turning them into real usable experiences. I'm not really a designer, but I do sometimes like to design things for myself—including this website!</p>
+						<p>I'm Zeth, a frontend developer with years of experience crafting engaging sites through the magical languages of HTML, CSS, JavaScript, PHP, and React JavaScript/TypeScript. I love taking beautiful, aesthetically pleasing designs and turning them into real usable experiences. I'm not really a designer, but I do sometimes like to design things for myself—including this website!</p>
 						<p>I've been at my current role as Frontend Developer at Studio Simpatico for about 5 years. I <em>have</em> to take a moment to share how lucky I feel to have ended up with this group of such amazing people, both professionally and personally! I've learned so much from them and had fun while doing it—the vibes are truly immaculate.</p>
 						<p>Outside of work, I enjoy eating good food, drinking good coffee, strengthening my body, and hanging out with my peeps. When the weather's nice, taking a drive down PCH to my little spot on the beach is my favorite way to clear the mind and live in the moment. When the money's nice, travelling to new places and exploring the food and nature is a must!</p>
 					</div>
--- src/components/pages/home/Intro.tsx
+++ src/components/pages/home/Intro.tsx
@@ -19,7 +19,7 @@ const Intro = () => {
 						</LunaScroll>
 					</h2>
 
-					<p>I'm Zeth, a frontend developer with years of experience crafting engaging sites through the magical languages of HTML, CSS, JavaScript, PHP, and a sprinkle of React JavaScript/TypeScript. I love taking beautiful, aesthetically pleasing designs and turning them into real usable experiences. Outside of work, I enjoy eating good food, drinking good coffee, strengthening my body, and hanging out with my peeps.</p>
+					<p>I'm Zeth, a frontend developer with years of experience crafting engaging sites through the magical languages of HTML, CSS, JavaScript, PHP, and React JavaScript/TypeScript. I love taking beautiful, aesthetically pleasing designs and turning them into real usable experiences. Outside of work, I enjoy eating good food, drinking good coffee, strengthening my body, and hanging out with my peeps.</p>
 
 				</article>
 
